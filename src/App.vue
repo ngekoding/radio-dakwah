16a6fbfb7ec08cd28ba5210b69ce937dd60d76d4
Nur Muhammad <blog.nurmuhammad@gmail.com>
2023-01-22 17:39:07 +0800

    Fix player height

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
 import RadioPlayer from '@/components/RadioPlayer.vue'
+
+const documentHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+
+window.addEventListener('resize', documentHeight)
+documentHeight()
 </script>
 
 <template>
