7d33426316e44759651e23bf086a3b517e49abb1
Nur Muhammad <blog.nurmuhammad@gmail.com>
2024-03-04 22:23:28 +0800

    Add PWA integration

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,47 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import ViewportHeightCorrection from 'postcss-viewport-height-correction'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/radio-dakwah/' : '/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['radio_black.svg'],
+      manifest: {
+        name: 'Radio Dakwah',
+        short_name: 'Radio Dakwah',
+        description: 'Menjaga Ketenteraman Hati',
+        theme_color: '#ffffff',
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,jpg,png,svg}']
+      },
+      pwaAssets: {
+        config: true,
+        overrideManifestIcons: true
+      },
+      devOptions: {
+        enabled: true,
+        suppressWarnings: true,
+        type: 'module'
+      }
+    })
+  ],
   css: {
     postcss: {
       plugins: [ViewportHeightCorrection({ variable: 'vh' })]
     }
   },
   server: {
-    port: 3333
+    port: 3333,
+    https: {
+      key: '/Users/nur/Devs/WebProjects/localhost-key.pem',
+      cert: '/Users/nur/Devs/WebProjects/localhost.pem',
+    }
   },
   resolve: {
     alias: {
